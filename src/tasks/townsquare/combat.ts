c98c64df1196fa3861930933e339ec82f48b8aea
Glup Shitto <frigley.kol@gmail.com>
2023-07-02 15:58:47 -0400

    Hardcode combats

--- src/tasks/townsquare/combat.ts
+++ src/tasks/townsquare/combat.ts
@@ -1,16 +1,19 @@
 
 import { Macro, $skill, $item } from "libram"
 
-const delevel = Macro.trySkill($skill`Curse of weaksauce`)
+export const mortar = Macro.trySkill($skill`Curse of weaksauce`)
 .trySkill($skill`Micrometeorite`)
 .tryItem($item`Rain-Doh indigo cup`)
 .trySkill($skill`Summon Love Mosquito`)
 .tryItem($item`Time-Spinner`)
-.tryItem($item`HOA citation pad`);
-
-export const mortar = delevel
+.tryItem($item`HOA citation pad`)
 .trySkill($skill`Stuffed Mortar Shell`)
 .tryItem($item`seal tooth`);
 
-export const attack = delevel
+export const attack = Macro.trySkill($skill`Curse of weaksauce`)
+.trySkill($skill`Micrometeorite`)
+.tryItem($item`Rain-Doh indigo cup`)
+.trySkill($skill`Summon Love Mosquito`)
+.tryItem($item`Time-Spinner`)
+.tryItem($item`HOA citation pad`)
 .attack();
