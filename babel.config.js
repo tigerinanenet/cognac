9a22f0935dcdd3506b695333d4a25e47c9dfec85
Patrick Hulin <patrick.hulin@gmail.com>
2023-07-03 17:24:04 -0400

    Prettier.

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,19 @@
 // eslint-disable-next-line no-undef
 module.exports = function (api) {
-    api.cache(true);
-    return {
-      presets: [
-        "@babel/preset-typescript",
-        [
-          "@babel/preset-env",
-          {
-            targets: { rhino: "1.7.13" },
-          },
-        ],
+  api.cache(true);
+  return {
+    presets: [
+      "@babel/preset-typescript",
+      [
+        "@babel/preset-env",
+        {
+          targets: { rhino: "1.7.13" },
+        },
       ],
-      plugins: [
-        "@babel/plugin-proposal-class-properties",
-        "@babel/plugin-proposal-object-rest-spread",
-      ],
-    };
+    ],
+    plugins: [
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-proposal-object-rest-spread",
+    ],
   };
+};
