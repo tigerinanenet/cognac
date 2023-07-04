3977ca2838b9bb2e96c1b83e494cfb604274df2c
Glup Shitto <frigley.kol@gmail.com>
2023-07-04 16:04:11 -0400

    Fix effect gathering.

--- src/lib/effects.ts
+++ src/lib/effects.ts
@@ -1,14 +1,22 @@
 import { Effect } from "kolmafia";
-import { $effect, have } from "libram";
+import { $effect, $skill, have } from "libram";
 
 export function basicEffects(): Effect[] {
-  return (
-    [
-      $effect`The Sonata of Sneakiness`,
-      $effect`Smooth Movements`,
-      $effect`Leash of Linguini`,
-      $effect`Empathy`,
-      $effect`Blood Bond`,
-    ].filter(have) ?? []
-  );
+  const effects = [];
+  if (have($skill`The Sonata of Sneakiness`)) {
+    effects.push($effect`The Sonata of Sneakiness`);
+  }
+  if (have($skill`Smooth Movement`)) {
+    effects.push($effect`Smooth Movements`);
+  }
+  if (have($skill`Leash of Linguini`)) {
+    effects.push($effect`Leash of Linguini`);
+  }
+  if (have($skill`Empathy of the Newt`)) {
+    effects.push($effect`Empathy`);
+  }
+  if (have($skill`Blood Bond`)) {
+    effects.push($effect`Blood Bond`);
+  }
+  return effects;
 }
