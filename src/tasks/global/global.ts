2e25376f69f9f818949561a6ac974d15d663b9e2
Glup Shitto <frigley.kol@gmail.com>
2023-07-06 13:05:33 -0400

    Support driving stealthily

--- src/tasks/global/global.ts
+++ src/tasks/global/global.ts
@@ -1,5 +1,6 @@
 import { Task } from "grimoire-kolmafia";
 import { getFuel } from "kolmafia";
+import { $effect } from "libram";
 
 import { $item, $location, AsdonMartin, AutumnAton, have } from "libram";
 
@@ -12,6 +13,15 @@ export const globalTasks = (): Task[] => {
       },
       do: () => {
         AsdonMartin.fillTo(50);
+      },
+    },
+    {
+      name: "Drive Stealthily ",
+      completed: () => {
+        return !AsdonMartin.installed() || have($effect`Driving Stealthily`);
+      },
+      do: () => {
+        AsdonMartin.drive($effect`Driving Stealthily`);
       },
     },
     {
