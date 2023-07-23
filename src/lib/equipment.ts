a9a671f3f4fd9feb7c7f82ec2a58bb4d40093ccb
Patrick Hulin <patrick.hulin@gmail.com>
2023-07-23 11:04:29 -0400

    add pantsgiving

--- src/lib/equipment.ts
+++ src/lib/equipment.ts
@@ -1,5 +1,5 @@
-import { inebrietyLimit, Item, myInebriety } from "kolmafia";
-import { $item, have } from "libram";
+import { inebrietyLimit, Item, myInebriety, toSlot } from "kolmafia";
+import { $item, $slot, get, have } from "libram";
 
 import { freeRunItemAvailable, shouldUseFreeRunItem } from "./freeRun";
 
@@ -28,6 +28,11 @@ export function getDefaultEquipment() {
     } else if (have($item`navel ring of navel gazing`)) {
       defaultEquipment.push($item`navel ring of navel gazing`);
     }
+  }
+
+  const pants = defaultEquipment.find((item) => toSlot(item) === $slot`pants`);
+  if ((!pants || !have(pants)) && get("_pantsgivingCount") < 500) {
+    defaultEquipment.push($item`Pantsgiving`);
   }
 
   return getEquipment(defaultEquipment);
