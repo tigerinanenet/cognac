5694f02cdcc98f9687dc7e7ab18659fd18833f45
Alium58 <bennelson3927@gmail.com>
2023-07-25 10:41:31 -0500

    send current stench level in chat

--- src/scripts/cognac_chatbot.js
+++ src/scripts/cognac_chatbot.js
@@ -2,19 +2,24 @@ const kolmafia = require("kolmafia");
 
 module.exports.main = function main(sender, message, channel) {
   if (channel == "/hobopolis" && sender == "Dungeon") {
+    let stench = kolmafia.getProperty("_cognac_heapStench");
     if (message.endsWith("moved some trash out of the Purple Light District.")) {
-      const stench = kolmafia.getProperty("_cognac_heapStench");
-      // keep stench undefined until updateGossip sets initial value to whiteboard
-      kolmafia.setProperty("_cognac_heapStench", stench == "" ? "" : parseInt(stench) + 1);
+      stench = parseInt(stench) + 1;
+      kolmafia.setProperty("_cognac_heapStench", stench);
+      kolmafia.chatPrivate(`Stench level changed to ${stench}`);
     } else if (message.endsWith("moved some compost out of The Heap.")) {
-      // keep stench undefined until updateGossip sets initial value to whiteboard
-      kolmafa.setProperty(
-        "_cognac_heapStench",
-        stench == "" ? "" : Math.max(0, parseInt(stench) - 1),
-      );
+      stench = Math.max(0, parseInt(stench) - 1);
+      kolmafa.setProperty("_cognac_heapStench", stench);
+      kolmafia.chatPrivate(`Stench level changed to ${stench}`);
     } else if (message.endsWith("went treasure-hunting in The Heap.")) {
       kolmafia.setProperty("_cognac_heapStench", 0);
+      kolmafia.chatPrivate(`Stench level changed to 0`);
       kolmafia.setProperty("_lastDiver", message.split(" ")[0]);
+    }
+  } else if (channel == "/hobopolis" && sender != "Dungeon") {
+    const stench = kolmafia.getProperty("_cognac_heapStench");
+    if (stench == "" && message.startsWith("Stench level changed to ")) {
+      // todo: set stench level here. Need regex
     }
   }
 };
