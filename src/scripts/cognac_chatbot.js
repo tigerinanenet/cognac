5349743d0672a35ead0d18cf0da8a162171c7004
Alium58 <bennelson3927@gmail.com>
2023-07-26 07:12:11 -0500

    fix

--- src/scripts/cognac_chatbot.js
+++ src/scripts/cognac_chatbot.js
@@ -7,13 +7,14 @@ module.exports.main = function main(sender, message, channel) {
       stench = parseInt(stench) + 1;
       kolmafia.setProperty("_cognac_heapStench", stench);
       const targetStench = 8 + kolmafia.getProperty("_cognac_currentPlayers");
-      if (stench % (targetStench / 4) == 0 || stench >= targetStench) {
+      if (stench % Math.ceil(targetStench / 4) == 0 || stench >= targetStench) {
         kolmafia.chatClan(`Stench level changed to ${stench}`, "hobopolis");
       }
     } else if (message.endsWith("moved some compost out of the Heap.") && stench != "") {
       stench = Math.max(0, parseInt(stench) - 1);
       kolmafia.setProperty("_cognac_heapStench", stench);
-      if (stench % (targetStench / 4) == 0 || stench >= targetStench) {
+      const targetStench = 8 + kolmafia.getProperty("_cognac_currentPlayers");
+      if (stench % Math.ceil(targetStench / 4) == 0 || stench >= targetStench) {
         kolmafia.chatClan(`Stench level changed to ${stench}`, "hobopolis");
       }
     } else if (message.endsWith("went treasure-hunting in The Heap.")) {
